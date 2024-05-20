6cdf690e66199b08c4a718e1d95bdb6b257e54b9
Joseph Weissman <jweissman@users.noreply.github.com>
2024-05-20 14:52:55 -0400

    Update install.sh
    add common aliases

--- install.sh
+++ install.sh
@@ -4,6 +4,24 @@
 curl -s https://ohmyposh.dev/install.sh | sudo bash -s
 sudo oh-my-posh font install
 echo "eval \"\$(oh-my-posh init bash)\"" >> ~/.bashrc
+
+# Create .aliases file and add aliases
+cat <<EOL >> ~/.aliases
+alias gst='git status'
+alias gc='git commit'
+alias gd='git diff'
+alias glg='git log --graph --oneline'
+alias ga='git add'
+alias gco='git checkout'
+alias hb='gh pr view --web'                                                                                                                                          
+EOL
+
+# Add a line to .bashrc to source the .aliases file
+echo "if [ -f ~/.aliases ]; then" >> ~/.bashrc
+echo "  . ~/.aliases" >> ~/.bashrc
+echo "fi" >> ~/.bashrc
+
+# Reload the shell to apply changes
 exec bash
 
 
